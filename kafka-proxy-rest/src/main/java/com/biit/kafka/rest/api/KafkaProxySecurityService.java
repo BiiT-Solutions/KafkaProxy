3b211e12f70e3110aa93654bd7ba9b4c14543743
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-24 11:23:31 +0200

    Adapted to new SecurityService

--- kafka-proxy-rest/src/main/java/com/biit/kafka/rest/api/KafkaProxySecurityService.java
+++ kafka-proxy-rest/src/main/java/com/biit/kafka/rest/api/KafkaProxySecurityService.java
@@ -1,16 +1,12 @@
 package com.biit.kafka.rest.api;
 
 
-import com.biit.server.controllers.models.CreatedElementDTO;
-import com.biit.server.persistence.entities.CreatedElement;
 import com.biit.server.rest.SecurityService;
 import com.biit.server.security.IUserOrganizationProvider;
 import com.biit.server.security.model.IUserOrganization;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 
 @Primary
@@ -27,10 +23,8 @@ public class KafkaProxySecurityService extends SecurityService {
     private String editorPrivilege = null;
     private String organizationAdminPrivilege = null;
 
-    private final List<IUserOrganizationProvider<? extends IUserOrganization>> userOrganizationProviders;
-
     public KafkaProxySecurityService(List<IUserOrganizationProvider<? extends IUserOrganization>> userOrganizationProviders) {
-        this.userOrganizationProviders = userOrganizationProviders;
+        super(userOrganizationProviders);
     }
 
     @Override
@@ -63,53 +57,5 @@ public class KafkaProxySecurityService extends SecurityService {
             organizationAdminPrivilege = ORGANIZATION_ADMIN.toUpperCase();
         }
         return organizationAdminPrivilege;
-    }
-
-
-    public void canBeDoneByOrganizationAdmin(CreatedElementDTO dto, Authentication authentication) {
-        if (!userOrganizationProviders.isEmpty()) {
-            super.canBeDoneByOrganizationAdmin(dto, authentication, userOrganizationProviders.get(0));
-        }
-    }
-
-
-    public void canBeDoneByOrganizationAdmin(CreatedElement entity, Authentication authentication) {
-        if (!userOrganizationProviders.isEmpty()) {
-            super.canBeDoneByOrganizationAdmin(entity, authentication, userOrganizationProviders.get(0));
-        }
-    }
-
-    public void canBeDoneByDifferentUsers(Collection<String> userNames, Authentication authentication) {
-        if (!userOrganizationProviders.isEmpty()) {
-            super.canBeDoneByDifferentUsers(userNames, authentication, userOrganizationProviders.get(0));
-        }
-    }
-
-    public void canBeDoneByDifferentUsers(String userName, Authentication authentication) {
-        if (!userOrganizationProviders.isEmpty()) {
-            super.canBeDoneByDifferentUsers(userName, authentication, userOrganizationProviders.get(0));
-        }
-    }
-
-    public void checkHasOrganizationAdminAccess(String organizationName, Authentication authentication,
-                                                String... otherAuthoritiesAllowed) {
-        if (!userOrganizationProviders.isEmpty()) {
-            super.checkHasOrganizationAdminAccess(organizationName, authentication, userOrganizationProviders.get(0), otherAuthoritiesAllowed);
-        }
-    }
-
-    public IUserOrganization getRequiredUserOrganization(Authentication authentication,
-                                                         String... otherAuthoritiesAllowed) {
-        if (!userOrganizationProviders.isEmpty()) {
-            return super.getRequiredUserOrganization(authentication, userOrganizationProviders.get(0), otherAuthoritiesAllowed);
-        }
-        return null;
-    }
-
-    public void checkCreatedOn(CreatedElementDTO elementDTO, Authentication authentication,
-                               String... otherAuthoritiesAllowed) {
-        if (!userOrganizationProviders.isEmpty()) {
-            super.checkCreatedOn(elementDTO, authentication, userOrganizationProviders.get(0), otherAuthoritiesAllowed);
-        }
     }
 }
